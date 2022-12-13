c77a9a4cd9922eacf762b9ca36bc7b3c93ac478a
Eladio Júnior <eladiojunior@gmail.com>
2022-12-12 23:53:49 -0300

    Implementação de projeto de lista de tarefas em Flutter.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lista de Tarefas
+
+Projeto de Lista de Tarefas em Flutter.
+
+Com adaptações para ateder minhar chatices.
---
+++ android/app/src/main/kotlin/br/com/devd2/lista_tarefas_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.devd2.lista_tarefas_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
